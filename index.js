dea35881c75e36aa4414d0969b7fa30719e76ba0
ntnyq <ntnyq13@gmail.com>
2019-05-07 18:22:44 +0800

    build: add prettier config

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const beautifyHtml = require("js-beautify").html;
-const through2 = require("through2").obj;
+const beautifyHtml = require('js-beautify').html
+const through2 = require('through2').obj
 
 module.exports = function(options = {}) {
-  options.indent_size = options.indent_size || 2;
-  options.indent_char = options.indent_char || " ";
-  options.inline = options.inline || [];
+  options.indent_size = options.indent_size || 2
+  options.indent_char = options.indent_char || ' '
+  options.inline = options.inline || []
 
   return through2(function(file, encoding, callback) {
-    file.contents = Buffer.from(
-      beautifyHtml(file.contents.toString(), options)
-    );
-    callback(null, file);
-  });
-};
+    file.contents = Buffer.from(beautifyHtml(file.contents.toString(), options))
+    callback(null, file)
+  })
+}
